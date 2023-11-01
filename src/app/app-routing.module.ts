a5a38fd9062811637aacaafcc17515e3c4959d67
laradelrio <lara.delrio333@gmail.com>
2023-11-01 11:51:05 +0100

    angular project base structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './layout/mainpage/mainpage.component';
+import { HomeModule } from '@modules/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainpageComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('@modules/home/home.module').then((m):typeof HomeModule => m.HomeModule)
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainpageComponent } from './layout/mainpage/mainpage.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    CoreModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
